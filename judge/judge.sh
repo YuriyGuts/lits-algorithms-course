8d91fb5189a2a09db666fcedeede3f4f3110e745
Yuriy Guts <yuriy.guts@gmail.com>
2015-06-05 01:29:05 +0300

    Judge: added a possibility to test a single student. Used a more tidy layout for output files.

--- judge/judge.sh
+++ judge/judge.sh
@@ -21,38 +21,46 @@ do
     STUDENT_NAME=${STUDENT_DIR%*/}
     STUDENT_NAME=${STUDENT_NAME##*/}
 
-    echo ""
-    echo "Testing student: $STUDENT_NAME"
+    if [ -z "$2" ] || [ "$2" == "$STUDENT_NAME" ]; then
+        echo ""
+        echo "Testing student: $STUDENT_NAME"
 
-    cd $STUDENT_DIR
+        cd $STUDENT_DIR
 
-    # Clean files from the previous run.
-    rm *.in *.out *.log 2>/dev/null
+        # Clean files from the previous run.
+        rm -rf output 2>/dev/null
+        mkdir output
+        mkdir output/stdout
+        mkdir output/stderr
 
-    # Compile student's solution.
-    if [ -f compile.sh ]
-    then
-        echo "    Compiling"
-        bash compile.sh
+        # Compile student's solution.
+        if [ -f compile.sh ]
+        then
+            echo "    Compiling"
+            bash compile.sh
+        fi
+
+        # Iterate over test cases.
+        for TEST_CASE in $PROBLEM_DIR/testcases/*.in
+        do
+            TEST_CASE_BASENAME="`basename $TEST_CASE`"
+            TEST_CASE_NUMBER="${TEST_CASE_BASENAME%.*}"
+
+            echo "    Running test case $TEST_CASE_NUMBER"
+
+            # Symlinking the testcase: student's solution expects a hardcoded file name.
+            ln -f -s $TEST_CASE $PROBLEM_CODE.in
+
+            # Run student's solution.
+            bash run.sh 1>./output/stdout/$TEST_CASE_NUMBER-stdout.log 2>./output/stderr/$TEST_CASE_NUMBER-stderr.log
+
+            # Saving the output for the test case.
+            mv -f $PROBLEM_CODE.out ./output/$TEST_CASE_NUMBER.out
+        done
+
+        # Removing the symlink.
+        rm $PROBLEM_CODE.in
     fi
-
-    # Iterate over test cases.
-    for TEST_CASE in $PROBLEM_DIR/testcases/*.in
-    do
-        TEST_CASE_BASENAME="`basename $TEST_CASE`"
-        TEST_CASE_NUMBER="${TEST_CASE_BASENAME%.*}"
-
-        echo "    Running test case $TEST_CASE_NUMBER"
-
-        # Symlinking the testcase: student's solution expects a hardcoded file name.
-        ln -f -s $TEST_CASE $PROBLEM_CODE.in
-
-        # Run student's solution.
-        bash run.sh 1>$TEST_CASE_NUMBER-stdout.log 2>$TEST_CASE_NUMBER-stderr.log
-
-        # Saving the output for the test case.
-        mv -f $PROBLEM_CODE.out $TEST_CASE_NUMBER.out
-    done
 done
 
 cd $ROOT_DIR
