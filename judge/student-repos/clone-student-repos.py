92dc19e01b78c42641b7873a823cc9a20da12b80
Yuriy Guts <yuriy.guts@gmail.com>
2016-04-06 13:39:45 +0300

    Moved grader-job.conf out of version control.

--- judge/student-repos/clone-student-repos.py
+++ judge/student-repos/clone-student-repos.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
+import json
 import subprocess
 import sys
 
 
 def main():
-    student_repos = {
-        "johndoe": "https://github.com/johndoe/algorithms",
-        "janedoe": "https://github.com/janedoe/algorithms"
-    }
+    with open("grader-job.conf", "r") as job_conf_file:
+        job_conf = json.loads(job_conf_file.read())
+        student_repos = job_conf["student_repos"]
 
     for name, url in student_repos.iteritems():
         subprocess.call("git clone {url} {name}".format(**locals()), shell=True)
