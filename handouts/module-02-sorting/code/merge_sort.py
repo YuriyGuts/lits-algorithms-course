cf9b85bf4c6911c2c238cac7e402216c42b65265
Yuriy Guts <yuriy.guts@gmail.com>
2016-04-26 22:04:11 +0300

    Module 02: Added a section on k-order statistics.

--- handouts/module-02-sorting/code/merge_sort.py
+++ handouts/module-02-sorting/code/merge_sort.py
@@ -1,49 +1,49 @@
 def sort(array):
-    result = merge_sort_recursive(array, 0, len(array) - 1)
-
-    # Replacing the input array with the sorted result.
-    for i in range(0, len(result)):
-        array[i] = result[i]
+    # Creating an auxiliary array for storing the merged result.
+    merge_results = [0] * len(array)
+    merge_sort_recursive(array, merge_results, 0, len(array) - 1)
 
 
-def merge_sort_recursive(array, left, right):
+def merge_sort_recursive(array, merge_results, left, right):
     if left < right:
-        middle = (left + right) / 2
-
+        middle = (left + right) // 2
+        
         # Splitting the larger task into smaller subtasks and solving each recursively.
-        left_part = merge_sort_recursive(array, left, middle)
-        right_part = merge_sort_recursive(array, middle + 1, right)
+        merge_sort_recursive(array, merge_results, left, middle)
+        merge_sort_recursive(array, merge_results, middle + 1, right)
 
         # Merging the results of the subproblems.
-        result = merge(left_part, right_part)
-
-        return result
-
-    # If our [left..right] range contains only one item, returning it.
-    return [array[left]]
+        merge(array, merge_results, left, middle + 1, right)
 
 
-def merge(left_array, right_array):
-    # Creating an auxiliary array for storing the merged result.
-    result = [None] * (len(left_array) + len(right_array))
-
+def merge(array, merge_results, left_begin, right_begin, right_end):
     # Remembering the reading positions for both subarrays.
-    left_pos = right_pos = result_pos = 0
+    left_end = right_begin - 1
+    left_read_pos = left_begin
+    right_read_pos = right_begin
+    result_write_pos = left_begin
 
     # Picking items either from the left subarray or the right subarray,
     # depending on which item is smaller.
-    while result_pos < len(result):
-        # If we have no more items on the left OR right item is smaller than left
-        # then pick from the right subarray.
-        if left_pos >= len(left_array) or (right_pos < len(right_array) and 
-                compare(right_array[right_pos], left_array[left_pos])):
-            result[result_pos] = right_array[right_pos]
-            right_pos += 1
-        # ...otherwise, pick from the left one.
+    while left_read_pos <= left_end and right_read_pos <= right_end:
+        if compare(array[left_read_pos], array[right_read_pos]):
+            merge_results[result_write_pos] = array[left_read_pos]
+            left_read_pos += 1
         else:
-            result[result_pos] = left_array[left_pos]
-            left_pos += 1
+            merge_results[result_write_pos] = array[right_read_pos]
+            right_read_pos += 1
+        result_write_pos += 1
 
-        result_pos += 1
+    # If we have no more items on the right then pick from the left subarray.
+    while left_read_pos <= left_end:
+        merge_results[result_write_pos] = array[left_read_pos]
+        left_read_pos += 1
+        result_write_pos += 1
 
-    return result
+    # If we have no more items on the left then pick from the right subarray.
+    while right_read_pos <= right_end:
+        merge_results[result_write_pos] = array[right_read_pos]
+        right_read_pos += 1
+        result_write_pos += 1
+
+    array[left_begin:right_end + 1] = merge_results[left_begin:right_end + 1]
